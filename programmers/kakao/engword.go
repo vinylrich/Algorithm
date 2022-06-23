14148a300a5976cbb655b52f6b8589ffa372ea7a
vinylrich <whktjd0109@gmail.com>
2022-06-23 16:00:33 +0900

    카카오 채용연계 인턴쉽 - 숫자 문자열과 영단어

--- programmers/kakao/engword.go
+++ programmers/kakao/engword.go
@@ -2,21 +2,40 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func solution(s string) int {
 
-	var a, b int
-	a = strings.Index(s, "zero")
-	b = strings.LastIndex(s, "one")
-
-	fmt.Println(a, b)
-	return a
-
+	//f := strings.NewReplacer(
+	//	"zero", "0",
+	//	"one", "1",
+	//	"two", "2",
+	//	"three", "3",
+	//	"four", "4",
+	//	"five", "5",
+	//	"six", "6",
+	//	"seven", "7",
+	//	"eight", "8",
+	//	"nine", "9",
+	//)
+	//i, _ := strconv.Atoi(f.Replace(s))
+	//return i
+	s = strings.Replace(s, "zero", "0", -1)
+	s = strings.Replace(s, "one", "1", -1)
+	s = strings.Replace(s, "two", "2", -1)
+	s = strings.Replace(s, "three", "3", -1)
+	s = strings.Replace(s, "four", "4", -1)
+	s = strings.Replace(s, "five", "5", -1)
+	s = strings.Replace(s, "six", "6", -1)
+	s = strings.Replace(s, "seven", "7", -1)
+	s = strings.Replace(s, "eight", "8", -1)
+	s = strings.Replace(s, "nine", "9", -1)
+	convInt, _ := strconv.Atoi(s)
+	return convInt
 }
 func main() {
 	s := "one4seveneightzero"
-	a := solution(s)
-	fmt.Println(a)
+	fmt.Println(solution(s))
 }
